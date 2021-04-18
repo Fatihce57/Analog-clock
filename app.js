3c45fa1fb50b97db88bda61a7ea49b232768fde6
Fatih Cengiz <fatihce57@gmail.com>
2021-04-18 02:14:18 +0200

    update

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const min = document.querySelector(".mins");
 const hours = document.querySelector(".hours");
 
 function tiktak() {
-let second = new Date().getSeconds();
-let minute = new Date().getMinutes();
-let hour = new Date().getHours();
+    let second = new Date().getSeconds();
+    let minute = new Date().getMinutes();
+    let hour = new Date().getHours();
 
-sec.style.transform = `rotate(${180 + (second * 6)}deg)`
-min.style.transform = `rotate(${180 + (minute * 6)}deg)`
-hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
-
-
+    sec.style.transform = `rotate(${180 + (second * 6)}deg)`
+    min.style.transform = `rotate(${180 + (minute * 6)}deg)`
+    hours.style.transform = `rotate(${180 + (hour * 30)}deg)`
 
 
-console.log({second,minute,hour})
+
+
+    console.log({ second, minute, hour })
 }
 
 setInterval(tiktak, 1000);
